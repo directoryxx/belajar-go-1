2be7094036a8dee3b8ff5b0c920f914af7ae609d
directoryx <anggawijaya4567@gmail.com>
2021-06-20 12:12:48 +0700

    initial commit

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Angga")
+	fmt.Println("Angga W")
+	fmt.Println("Angga Wijaya")
+	fmt.Println("Jumlah kata angga = ", len("angga"))
+	fmt.Println("Kata ketiga dari angga = ", string("angga"[2]))
+}
---
+++ variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name, uang string
+
+	name = "Angga"
+	uang = "banyak"
+
+	fmt.Println(name)
+	fmt.Println(uang)
+
+	name = "Angga W"
+	fmt.Println(name)
+
+	// Langsung Init var tanpa tipe data
+	var namaku = "angga"
+	fmt.Println(namaku)
+
+	// Ini variabel tanpa var
+	namakuini := "angga"
+	fmt.Println(namakuini)
+}

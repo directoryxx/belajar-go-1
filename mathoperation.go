e2a2a38acfc39380372ca6e28ac26f717406c99b
directoryx <anggawijaya4567@gmail.com>
2021-06-20 12:55:14 +0700

    math operation augmentented assignment

--- mathoperation.go
+++ mathoperation.go
@@ -2,23 +2,34 @@ package main
 
 import "fmt"
 
-func main()  {
+func main() {
 	var (
 		a int = 10
 		b int = 2
-		addition int
-		substraction int
-		multiplication int
-		division int
+		// addition int
+		// substraction int
+		// multiplication int
+		// division int
 	)
 
-	addition = a + b
-	substraction = a - b 
-	multiplication = a * b
-	division = a / b
+	// Old way
+	// addition = a + b
+	// substraction = a - b
+	// multiplication = a * b
+	// division = a / b
 
-	fmt.Println(addition)
-	fmt.Println(substraction)
-	fmt.Println(multiplication)
-	fmt.Println(division)
+	// New Way
+	a += b
+	fmt.Println(a)
+	a -= b
+	fmt.Println(a)
+	a *= b
+	fmt.Println(a)
+	a /= b
+	fmt.Println(a)
+
+	// fmt.Println(addition)
+	// fmt.Println(substraction)
+	// fmt.Println(multiplication)
+	// fmt.Println(division)
 }

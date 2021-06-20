bf741f5a3024606efe6b28189a892cf39b5adfb3
directoryx <anggawijaya4567@gmail.com>
2021-06-20 12:16:30 +0700

    mass create variable

--- variable.go
+++ variable.go
@@ -21,4 +21,25 @@ func main() {
 	// Ini variabel tanpa var
 	namakuini := "angga"
 	fmt.Println(namakuini)
+
+	//Define variabel langsung banyak tidak assign value
+	var (
+		firstName string
+		lastName  string
+	)
+
+	firstName = "Angga"
+	lastName = "Wijaya"
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+
+	//Define variabel langsung banyak dengan assign value
+	var (
+		firstName1 = "Angga"
+		lastName1  = "Wijaya"
+	)
+
+	fmt.Println(firstName1)
+	fmt.Println(lastName1)
 }

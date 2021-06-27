7a9a12d563ccaeaf7f5112a713dbd1afeb4bdc54
directoryx <anggawijaya4567@gmail.com>
2021-06-27 20:21:54 +0700

    access modifier

--- helper/sayhello.go
+++ helper/sayhello.go
@@ -2,7 +2,17 @@ package helper
 
 import "fmt"
 
+// Access modifier kayak private,public,protected
+
+var version = "1.0.0" // tidak bisa diexport karena huruf kecil
+var Application = "golang"
+
 // Function tidak boleh sama jika dalam satu package
 func SayHello(name string) {
 	fmt.Println("Halo", name)
+}
+
+// Tidak bisa diexport karena huruf kecil
+func sayGoodBye(name string) {
+	fmt.Println("Bye", name)
 }
--- import.go
+++ import.go
@@ -1,7 +1,12 @@
 package main
 
-import "go-with-compose/helper"
+import (
+	"fmt"
+	"go-with-compose/helper"
+)
 
 func main() {
 	helper.SayHello("angga")
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // Error karena diawali huruf kecil
 }

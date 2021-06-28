38259a186be781617fb356efc0d544798e7be6d7
directoryx <anggawijaya4567@gmail.com>
2021-06-28 12:25:06 +0700

    package math

---
+++ src/go-with-compose/math.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Round(1.4))
+	fmt.Println(math.Ceil(1.3))
+	fmt.Println(math.Max(2,120))
+	fmt.Println(math.Min(100,250))
+}
